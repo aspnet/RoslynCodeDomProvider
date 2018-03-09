e79aa12821414f33f88acde5532e1ee5afde2489
Benedek Farkas <benedek.farkas@lombiq.com>
2018-03-09 19:07:22 +0100

    Adding the ability to define a custom (local) path to Roslyn (#25) (#26)
    * CompilationSettings: C# 6 code styling and making CompilationSettingsHelper.CompilerFullPath public
    
    * Compiler: Adding the ability to define a custom compiler path for the current build process
    
    * Compiler: Fixing that the compiler path wasn't set when the override parameter is not present

---
+++ src/Microsoft.CodeDom.Providers.DotNetCompilerPlatform/Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.CodeDom.Providers.DotNetCompilerPlatform {
+    /// <summary>
+    /// Common, reusable constant values.
+    /// </summary>
+    public static class Constants {
+        /// <summary>
+        /// Custom compiler parameters used to modify the behavior of the compiler service.
+        /// </summary>
+        public static class CustomCompilerParameters {
+            /// <summary>
+            /// Roslyn path override.
+            /// </summary>
+            public const string CustomCompilerPath = "compilerPath";
+        }
+    }
+}
