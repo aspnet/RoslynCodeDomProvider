05195ae7530e8919d79c57fd2b16e7e4a8ce9d8b
Taylor Southwick <tasou@microsoft.com>
2019-10-30 16:00:16 -0700

    Add ps1 files back

---
+++ src/Packages/Microsoft.CodeDom.Providers.DotNetCompilerPlatform/tools/install.ps1
@@ -0,0 +1,75 @@
+# Copyright (c) .NET Foundation. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+param($installPath, $toolsPath, $package, $project)
+
+$roslynSubFolder = 'roslyn'
+
+if ($project -eq $null) {
+    $project = Get-Project
+}
+
+$libDirectory = Join-Path $installPath 'lib\net45'
+$projectRoot = $project.Properties.Item('FullPath').Value
+$projectTargetFramework = $project.Properties.Item('TargetFrameworkMoniker').Value
+$shouldUseRoslyn45 = $projectTargetFramework -match '4.5'
+$binDirectory = Join-Path $projectRoot 'bin'
+
+# We need to copy the provider assembly into the bin\ folder, otherwise
+# Microsoft.VisualStudio.Web.Host.exe cannot find the assembly.
+# However, users will see the error after they clean solutions.
+New-Item $binDirectory -type directory -force | Out-Null
+Copy-Item $libDirectory\* $binDirectory -force | Out-Null
+
+# For Web Site, we need to copy the Roslyn toolset into
+# the applicaiton's bin folder. 
+# For Web Applicaiton project, this is done in csproj.
+if ($project.Type -eq 'Web Site') {
+    $packageDirectory = Split-Path $installPath
+
+    if($package.Versions -eq $null)
+    {
+        $compilerVersion = $package.Version
+    }
+    else
+    {
+		$compilerVersion = @($package.Versions)[0]
+    }
+
+    $compilerPackageFolderName = $package.Id + "." + $compilerVersion
+    $compilerPackageDirectory = Join-Path $packageDirectory $compilerPackageFolderName
+    if ((Get-Item $compilerPackageDirectory) -isnot [System.IO.DirectoryInfo])
+    {
+        Write-Host "The install.ps1 cannot find the installation location of package $compilerPackageName, or the pakcage is not installed correctly."
+        Write-Host 'The install.ps1 did not complete.'
+        break
+    }
+
+    if($shouldUseRoslyn45)
+    {
+        $compilerPackageToolsDirectory = Join-Path $compilerPackageDirectory 'tools\roslyn45'
+    }
+    else
+    {
+        $compilerPackageToolsDirectory = Join-Path $compilerPackageDirectory 'tools\roslynlatest'
+    }
+    $roslynSubDirectory = Join-Path $binDirectory $roslynSubFolder
+    New-Item $roslynSubDirectory -type directory -force | Out-Null
+    Copy-Item $compilerPackageToolsDirectory\* $roslynSubDirectory -force | Out-Null
+
+    # Generate a .refresh file for each dll/exe file.
+    Push-Location
+    Set-Location $projectRoot
+    $relativeAssemblySource = Resolve-Path -relative $compilerPackageToolsDirectory
+    Pop-Location
+
+    Get-ChildItem -Path $roslynSubDirectory | `
+    Foreach-Object {
+        if  (($_.Extension -eq ".dll") -or ($_.Extension -eq ".exe")) {
+            $refreshFile = $_.FullName
+            $refreshFile += ".refresh"
+            $refreshContent = Join-Path $relativeAssemblySource $_.Name    
+            Set-Content $refreshFile $refreshContent
+        }
+    }
+}
