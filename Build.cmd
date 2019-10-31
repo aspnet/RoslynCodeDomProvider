f78434cc2ee2260945464a9eea57ca98f056472c
Steve Molloy <smolloy@microsoft.com>
2019-10-31 10:42:55 -0700

    Add clean.cmd and address a couple other project nits. (#91)

--- Build.cmd
+++ Build.cmd
@@ -1,20 +1,21 @@
 @ECHO OFF
 
 setlocal
-
 set EnableNuGetPackageRestore=true
+
+set MSBUILDEXE=msbuild.exe
 
 set cfgOption=/p:Configuration=Release
 REM set cfgOption=/p:Configuration=Debug
 REM set cfgOption=/p:Configuration=Debug;Release
 if not "%1"=="" set cfgOption=/p:Configuration=
 
-REM set logOptions=/v:diag /flp:Summary;Verbosity=normal;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
 set logOptions=/v:n /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+REM set logOptions=/v:diag /flp:Summary;Verbosity=normal;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
 
 echo Please build from VS 2015(or newer version) Developer Command Prompt
 
-msbuild "%~dp0\RoslynCodeProvider.msbuild" %logOptions% /maxcpucount /nodeReuse:false %cfgOption%
+%MSBUILDEXE% "%~dp0\RoslynCodeProvider.msbuild" /t:BuildAll %logOptions% /maxcpucount /nodeReuse:false %cfgOption%%*
 if %ERRORLEVEL% neq 0 goto BuildFail
 goto BuildSuccess
 
---
+++ Clean.cmd
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+setlocal
+
+set MSBUILDEXE=msbuild.exe
+
+set cfgOption=/p:Configuration=Release
+REM set cfgOption=/p:Configuration=Debug
+REM set cfgOption=/p:Configuration=Debug;Release
+if not "%1"=="" set cfgOption=/p:Configuration=
+
+set logOptions=/v:n /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+REM set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+
+%MSBUILDEXE% "%~dp0\RoslynCodeProvider.msbuild" /t:Clean %logOptions% /maxcpucount /nodeReuse:false %cfgOption%%*
+del /F msbuild.log
+del /F msbuild.wrn
+del /F msbuild.err
+
+endlocal
