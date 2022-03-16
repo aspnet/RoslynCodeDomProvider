8b0cd372720d4b3191d4d310e9403fcad4e34e74
Steve Molloy <smolloy@microsoft.com>
2022-03-16 15:38:32 -0700

    Update license headers. Also ProjectUrl.

--- src/Microsoft.CodeDom.Providers.DotNetCompilerPlatform/Util/ProviderOptions.cs
+++ src/Microsoft.CodeDom.Providers.DotNetCompilerPlatform/Util/ProviderOptions.cs
@@ -1,7 +1,7 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Microsoft.CodeDom.Providers.DotNetCompilerPlatform {
