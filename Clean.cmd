e79da36ac4f10ba060976bd3c67be7a0b1e1c0b0
Steve Molloy <smolloy@microsoft.com>
2025-03-27 11:13:05 -0700

    Fix script signing in nuget packages

--- Clean.cmd
+++ Clean.cmd
@@ -13,13 +13,22 @@ set logOptions=/v:n /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warnin
 REM set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
 
 %MSBUILDEXE% "%~dp0\RoslynCodeProvider.msbuild" /t:Clean %logOptions% /maxcpucount /nodeReuse:false %cfgOption%%*
+
+pushd %~dp0\src\RoslynCodeProviderTest
+rd /q /s bin
+rd /q /s obj
+popd
+pushd %~dp0\src\DotNetCompilerPlatformTasks
+rd /q /s bin
+rd /q /s obj
+popd
 pushd %~dp0\src\Packages\Microsoft.CodeDom.Providers.DotNetCompilerPlatform
 rd /q /s tools
 rd /q /s tasks
-rd /q /s build.pp
+rd /q /s build\pp
 popd
 pushd %~dp0\src\Packages\Microsoft.CodeDom.Providers.DotNetCompilerPlatform.WebSites
-rd /q /s tools.pp
+rd /q /s tools\pp
 popd
 rd /q /s bin
 rd /q /s obj
